a0493c866371e28fce10bcca0b32a3cc8d7ef9b0
Jaehyun <heroinspace@hanmail.net>
2019-11-20 00:03:49 +0900

    finish #2 Project Setup
    #2.2 homework

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "Components/Header";
+import Router from "Components/Router";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  );
+}
+
+export default App;
