525d85143104def10a53aef2038c9e9063e5aa7f
Jaehyun <heroinspace@hanmail.net>
2019-11-23 00:56:54 +0900

    finish #3 Styles

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Header from "Components/Header";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 function App() {
   return (
     <>
-      <Header />
       <Router />
+      <GlobalStyles />
     </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,54 @@
 import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
 
-export default () => (
-  <header>
-    <ul>
-      <li>
-        <a href="/">Movies</a>
-      </li>
-      <li>
-        <a href="/tv">TV</a>
-      </li>
-      <li>
-        <a href="/search">Search</a>
-      </li>
-    </ul>
-  </header>
-);
+const Header = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  background-color: rgba(20, 20, 20, 0.8);
+  z-index: 10;
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.8);
+`;
+
+const List = styled.ul`
+  display: flex;
+`;
+
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  background-color: ${props =>
+    props.current ? "rgba(52, 152, 219, 0.2)" : "inherit"};
+  border-bottom: 3px solid
+    ${props => (props.current ? "#3498db" : "transparent")};
+  transition: background-color 0.5s ease-in-out, border-bottom 0.5s ease-in-out;
+`;
+
+const SLink = styled(Link)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default withRouter(({ location: { pathname } }) => (
+  <Header>
+    <List>
+      <Item current={pathname === "/"}>
+        <SLink to="/">Movies</SLink>
+      </Item>
+      <Item current={pathname === "/tv"}>
+        <SLink to="/tv">TV</SLink>
+      </Item>
+      <Item current={pathname === "/search"}>
+        <SLink to="/search">Search</SLink>
+      </Item>
+    </List>
+  </Header>
+));
